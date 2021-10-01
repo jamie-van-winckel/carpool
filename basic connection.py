2c38d5ad14160a293a143ec16e0ade0c6a72744c
ltokuno <lance@neemis.com>
2021-09-30 20:03:48 -0700

    changed to environment vars for tokens

--- basic connection.py
+++ basic connection.py
@@ -5,7 +5,11 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Install the Slack app and get xoxb- token in advance
-app = App(token="xoxb-2538212706071-2566324247489-uD06b3SlceDo675fWb4k1NiP")
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 if __name__ == "__main__":
-    SocketModeHandler(app, "xapp-1-A02GB9DJ5PW-2576821471776-4f5a3c562bee463a5c85caf44e9823b89a023476dfe931b1542bf7f7e6f53e9f").start()
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
+
+    
