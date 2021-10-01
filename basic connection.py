e813f34beeb5d4513b6f6d536ef4707179c69bc7
ltokuno <lance@neemis.com>
2021-10-01 11:17:10 -0700

    Added command handler and message response

--- basic connection.py
+++ basic connection.py
@@ -7,6 +7,12 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 # Install the Slack app and get xoxb- token in advance
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
+@app.command("/carpool1")
+def handle_some_command(ack, body, logger, say):
+    ack()
+    logger.info(body)
+    say('hello from carpool1')
+
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
