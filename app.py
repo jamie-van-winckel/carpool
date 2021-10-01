4464122ecd70b95b3f898dfc9c4e2d9a3a67f91e
ltokuno <lance@neemis.com>
2021-10-01 15:11:50 -0700

    Changed list to set in cache to have unique users per zip code

--- app.py
+++ app.py
@@ -7,31 +7,33 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 # Install the Slack app and get xoxb- token in advance
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
-cache = defaultdict(list)
+cache = defaultdict(set)
 
-with open('user_zips.json', 'r') as userZips_fp:
-    cache = json.load(userZips_fp)
-
+try:
+    with open('user_zips.json', 'r') as userZips_fp:
+        cache = json.load(userZips_fp)
+except IOError:
+    print("Error: file does not exist")
 
 @app.command("/carpool1")
 def carpool1_handler(ack, body, say):
     ack()
     say("Hello " + body['user_name'] + '!')
 
+    cache[body['text']].add(body['user_name']) 
 
-    cache[body['text']].append(body['user_name']) 
     #save the users to a file
     with open('user_zips.json', 'w') as userZips_fp:
-        json.dump(cache, userZips_fp, sort_keys=True, indent=4)
-
+        json.dump(list(cache), userZips_fp, sort_keys=True, indent=4)
 
     if (len(cache[body['text']]) < 2):
         say("You are the only person carpooling in " + body['text'])
     else:
-        say('You are close to:')
+        output = 'You are close to:'
         for u in cache[body['text']]:
             if (u != body['user_name']):
-                say(u)
+                output = output + " " + u
+        say(output)
 
 
 if __name__ == "__main__":
